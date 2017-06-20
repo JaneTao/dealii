904c8985a980ee2420a9f60f1beac97f571b47c3
JaneTao <bobosha@gmail.com>
2017-06-20 17:10:33 -0500

    np serendipity space

---
+++ include/deal.II/fe/fe_s_np_base.h
@@ -0,0 +1,151 @@
+#ifndef __deal2__fe_s_np_base_h
+#define __deal2__fe_s_np_base_h
+
+#include <deal.II/base/config.h>
+#include <deal.II/fe/fe_poly.h>
+#include <deal.II/base/thread_management.h>
+
+DEAL_II_NAMESPACE_OPEN
+
+template <class POLY, int dim=POLY::dimension, int spacedim=dim>
+class FE_S_NP_Base : public FE_Poly<POLY,dim,spacedim>
+{
+public:
+
+  FE_S_NP_Base (const POLY &poly_space,
+             const FiniteElementData<dim> &fe_data,
+             const std::vector<bool> &restriction_is_additive_flags);
+
+  virtual void
+  get_interpolation_matrix (const FiniteElement<dim,spacedim> &source,
+                            FullMatrix<double>       &matrix) const;
+
+  virtual void
+  get_face_interpolation_matrix (const FiniteElement<dim,spacedim> &source,
+                                 FullMatrix<double>       &matrix) const;
+
+  virtual void
+  get_subface_interpolation_matrix (const FiniteElement<dim,spacedim> &source,
+                                    const unsigned int        subface,
+                                    FullMatrix<double>       &matrix) const;
+
+  virtual bool has_support_on_face (const unsigned int shape_index,
+                                    const unsigned int face_index) const;
+
+  virtual const FullMatrix<double> &
+  get_restriction_matrix (const unsigned int child,
+                          const RefinementCase<dim> &refinement_case=RefinementCase<dim>::isotropic_refinement) const;
+
+  virtual const FullMatrix<double> &
+  get_prolongation_matrix (const unsigned int child,
+                           const RefinementCase<dim> &refinement_case=RefinementCase<dim>::isotropic_refinement) const;
+
+  virtual
+  unsigned int face_to_cell_index (const unsigned int face_dof_index,
+                                   const unsigned int face,
+                                   const bool face_orientation = true,
+                                   const bool face_flip        = false,
+                                   const bool face_rotation    = false) const;
+
+  virtual std::pair<Table<2,bool>, std::vector<unsigned int> >
+  get_constant_modes () const;
+
+  virtual bool hp_constraints_are_implemented () const;
+
+  virtual
+  std::vector<std::pair<unsigned int, unsigned int> >
+  hp_vertex_dof_identities (const FiniteElement<dim,spacedim> &fe_other) const;
+
+  virtual
+  std::vector<std::pair<unsigned int, unsigned int> >
+  hp_line_dof_identities (const FiniteElement<dim,spacedim> &fe_other) const;
+
+  virtual
+  std::vector<std::pair<unsigned int, unsigned int> >
+  hp_quad_dof_identities (const FiniteElement<dim,spacedim> &fe_other) const;
+
+  virtual
+  FiniteElementDomination::Domination
+  compare_for_face_domination (const FiniteElement<dim,spacedim> &fe_other) const;
+  //@}
+
+protected:
+  /**
+   * Only for internal use. Its full name is @p get_dofs_per_object_vector
+   * function and it creates the @p dofs_per_object vector that is needed
+   * within the constructor to be passed to the constructor of @p
+   * FiniteElementData.
+   */
+  static std::vector<unsigned int> get_dpo_vector(const unsigned int degree);
+
+  void initialize (const std::vector<Point<1> > &support_points_1d);
+
+  void initialize_constraints (const std::vector<Point<1> > &points);
+
+  void initialize_unit_support_points (const std::vector<Point<1> > &points);
+
+  void initialize_unit_face_support_points (const std::vector<Point<1> > &points);
+
+  void initialize_quad_dof_index_permutation ();
+
+  struct Implementation;
+
+  friend struct FE_S_NP_Base<POLY,dim,spacedim>::Implementation;
+
+  // ------------------------------------------------------------------
+  // by Zhen Tao
+
+  virtual UpdateFlags update_once (const UpdateFlags flags) const;
+ 
+  virtual UpdateFlags update_each (const UpdateFlags flags) const;
+
+  virtual
+  typename Mapping<dim,spacedim>::InternalDataBase *
+  get_data (const UpdateFlags,
+            const Mapping<dim,spacedim> &mapping,
+            const Quadrature<dim> &quadrature) const ;
+
+  virtual void
+  fill_fe_values (const Mapping<dim,spacedim>                       &mapping,
+                  const typename Triangulation<dim,spacedim>::cell_iterator &cell,
+                  const Quadrature<dim>                             &quadrature,
+                  typename Mapping<dim,spacedim>::InternalDataBase  &mapping_internal,
+                  typename Mapping<dim,spacedim>::InternalDataBase  &fe_internal,
+                  FEValuesData<dim,spacedim>                        &data,
+                  CellSimilarity::Similarity                   &cell_similarity) const;
+
+  virtual void
+  fill_fe_face_values (const Mapping<dim,spacedim> &mapping,
+                       const typename Triangulation<dim,spacedim>::cell_iterator &cell,
+                       const unsigned int                                  face_no,
+                       const Quadrature<dim-1>                            &quadrature,
+                       typename Mapping<dim,spacedim>::InternalDataBase   &mapping_internal,
+                       typename Mapping<dim,spacedim>::InternalDataBase   &fe_internal,
+                       FEValuesData<dim,spacedim> &data) const ;
+
+  virtual void
+  fill_fe_subface_values (const Mapping<dim,spacedim> &mapping,
+                          const typename Triangulation<dim,spacedim>::cell_iterator &cell,
+                          const unsigned int                    face_no,
+                          const unsigned int                    sub_no,
+                          const Quadrature<dim-1>                &quadrature,
+                          typename Mapping<dim,spacedim>::InternalDataBase      &mapping_internal,
+                          typename Mapping<dim,spacedim>::InternalDataBase      &fe_internal,
+                          FEValuesData<dim,spacedim> &data) const ;
+
+  class InternalData : public FiniteElement<dim,spacedim>::InternalDataBase
+  {
+
+  };
+
+
+private:
+  mutable Threads::Mutex mutex;
+};
+
+
+/*@}*/
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
